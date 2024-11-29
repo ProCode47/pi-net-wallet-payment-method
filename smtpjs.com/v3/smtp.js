0a3e803099f52ec90b0e08cf279217fd9181b7dd
DenSweed <echefrankidosky4love@gmail.com>
2024-11-30 00:24:59 +0100

    smtp.js

--- smtpjs.com/v3/smtp.js
+++ smtpjs.com/v3/smtp.js
@@ -1,27 +1,34 @@
-const Form = document.getElementById("my-form");
-const Names = document.getElementById("contact-inputs");
-
-async function sendEmail() {
-  const phrase = Names.value;
-
-  try {
-    const response = await fetch("/send-email", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ phrase }),
-    });
-
-    if (response.ok) {
-      document.getElementById("status").style.display = "block";
-    } else {
-      console.error("Failed to send email.");
+/* SmtpJS.com - v3.0.0 */
+var Email = {
+    send: function (a) {
+        return new Promise(function (n, e) {
+            a.nocache = Math.floor(1e6 * Math.random() + 1);
+            a.Action = "Send";
+            var t = JSON.stringify(a);
+            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+                n(e);
+            });
+        });
+    },
+    ajaxPost: function (e, n, t) {
+        var a = Email.createCORSRequest("POST", e);
+        a.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        a.onload = function () {
+            var e = a.responseText;
+            if (t) t(e);
+        };
+        a.send(n);
+    },
+    createCORSRequest: function (e, n) {
+        var t = new XMLHttpRequest();
+        if ("withCredentials" in t) {
+            t.open(e, n, true);
+        } else if (typeof XDomainRequest !== "undefined") {
+            t = new XDomainRequest();
+            t.open(e, n);
+        } else {
+            t = null;
+        }
+        return t;
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-Form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  sendEmail();
-});
+};
